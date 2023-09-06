feb0886cefef351fd1e3108e3795dc5c673da8fe
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2023-09-06 16:18:15 -0300

    criando projeto de menu e splashScreen

--- TelaApresentacao/app/src/main/java/com/ozika/ifto/telaapresentacao/MainActivity.java
+++ TelaApresentacao/app/src/main/java/com/ozika/ifto/telaapresentacao/MainActivity.java
@@ -4,13 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btn;
+    private int numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("thread3",Thread.currentThread().getName());
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(view -> {
+            numero = Integer.parseInt(editText.getText().toString());
+            gerarNumero(numero);
+        });
+    }
+
+    private void gerarNumero(int numjogador) {
+        Random r = new Random();
+        int numcomputador = r.nextInt(11);
+        if(numcomputador>numjogador){
+            Toast.makeText(this,"Android venceu "+numcomputador+" > "+numjogador,Toast.LENGTH_SHORT).show();
+        }else if(numcomputador==numjogador){
+            Toast.makeText(this,"Empate "+numcomputador+" = "+numjogador,Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"Jogador venceu "+numcomputador+" < "+numjogador,Toast.LENGTH_SHORT).show();
+        }
     }
 }
